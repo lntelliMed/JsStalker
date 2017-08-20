d006aacf6b215023d3c60bbf6b6f51a76eb75b0e
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-08-19 23:36:50 -0400

    Add logic to check whether the age is odd, perfect square or less than zero

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ console.log("You are " + age + " years old!");
 
 var days = age * 365.25;//Accounts for leap years!
 alert(age + " years is roughly " + days + " days!");
+var numberAge = Number(age);
+if (numberAge < 0){
+	console.log("Come back whenever you're born!");
+}
+
+if (numberAge === 21){
+	console.log("Happy 21st Birthday!");
+}
+
+if (numberAge % 2 !== 0){
+	console.log("Your age is an odd number!");
+}
+
+if (numberAge % Math.sqrt(numberAge) === 0){
+	console.log("Your age is a perfect square!");
+}
